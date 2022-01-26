f7e35ff94f417d2600e46bfc5de23b21f8fefac2
Renato Groffe <renato.groffe@yahoo.com.br>
2022-01-26 14:32:04 -0300

    Commit inicial
    Commit inicial

---
+++ Tests/tests-apicontagem.js
@@ -0,0 +1,15 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export let options = {
+    duration: '10s',
+    vus: 50,
+    thresholds: {
+        http_req_duration: ['p(95)<200']
+    }    
+};
+
+export default function() {
+    http.get('http://localhost:5000/contador');
+    sleep(1);
+};
